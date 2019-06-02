56a91a11f59f0b41bde5c50ea3061c6133a5fd6c
Pierre-Yves Rollo <dev@pyrollo.com>
2019-06-02 18:01:34 +0200

    Moved album description under thumbnails

--- zpbase/album-grid.php
+++ zpbase/album-grid.php
@@ -1,13 +1,13 @@
 <?php
-/*	zpBase album-grid.php 
-*	An actual album.php script utilizing default standard css grid.  
+/*	zpBase album-grid.php
+*	An actual album.php script utilizing default standard css grid.
 *	Will be included as the default album.php display, or can be called per album by the multiple layout plugin.
 *	http://www.oswebcreations.com
 ================================================== */
 
-$layoutbodyclass = 'body-defaultgrid'; 
+$layoutbodyclass = 'body-defaultgrid';
 include ('inc/header.php'); ?>
-			
+
 	<div class="container" id="middle">
 		<div class="row">
 			<div id="content">
@@ -33,7 +33,7 @@ include ('inc/header.php'); ?>
 						if (getOption('zpbase_social')) include ('inc/socialshare.php');
 						if ((class_exists('RSS')) && (getOption('RSS_album_image'))) { ?>
 						<span><?php printRSSLink('Collection','',gettext('Album RSS'),'',false); ?></span>
-						<?php } 
+						<?php }
 						} ?>
 						<?php if (getOption('zpbase_galss')) { ?>
 						<span><?php printBaseSlideShowLink(); ?></span>
@@ -41,10 +41,9 @@ include ('inc/header.php'); ?>
 						<span><?php printSlideShowLink(); ?></span>
 						<?php } ?>
 					</div>
-					<div id="object-desc"><?php printAlbumDesc(); ?></div>
 					<?php if (function_exists('printGoogleMap')) { ?><div id="map-wrap"><?php printGoogleMap('Google Map',null,'hide'); ?></div><?php } ?>
 				</div>
-					
+
 				<?php if (isAlbumPage()) { ?>
 				<div class="image-grid albums">
 					<?php while (next_album()): ?>
@@ -58,7 +57,7 @@ include ('inc/header.php'); ?>
 						</a>
 						<h3><?php printBareAlbumTitle();?></h3>
 						<p class="album-desc"><?php echo strip_tags(truncate_string(getAlbumDesc(),120,'...')); ?></p>
-					</div>	
+					</div>
 					<?php endwhile; ?>
 				</div>
 				<?php } ?>
@@ -70,8 +69,8 @@ include ('inc/header.php'); ?>
 							<a class="image-thumb popup-page" href="<?php echo html_encode(getImageURL()); ?>?show=imagepage" title="<?php printBareImageTitle();?>">
 								<?php if (getOption('thumb_crop')) {
 								printCustomSizedImage(getAnnotatedImageTitle(),getOption('thumb_size'),getOption('thumb_size'),getOption('thumb_size'),getOption('thumb_size'),getOption('thumb_size'),null,null,'remove-attributes',null,true);
-								} else { 
-								printImageThumb(getBareImageTitle(),'remove-attributes'); 
+								} else {
+								printImageThumb(getBareImageTitle(),'remove-attributes');
 								} ?>
 							</a>
 							<?php } elseif (isImagePhoto($_zp_current_image)) { ?>
@@ -79,35 +78,37 @@ include ('inc/header.php'); ?>
 							<?php } else { ?>
 								<?php if (getOption('thumb_crop')) {
 								printCustomSizedImage(getAnnotatedImageTitle(),getOption('thumb_size'),getOption('thumb_size'),getOption('thumb_size'),getOption('thumb_size'),getOption('thumb_size'),null,null,'remove-attributes',null,true);
-								} else { 
-								printImageThumb(getBareImageTitle(),'remove-attributes'); 
+								} else {
+								printImageThumb(getBareImageTitle(),'remove-attributes');
 								} ?>
 							<?php } ?>
 						<?php } else { ?>
 							<a class="image-thumb" href="<?php echo html_encode(getImageURL()); ?>" title="<?php printBareImageTitle();?>">
 								<?php if (getOption('thumb_crop')) {
 								printCustomSizedImage(getAnnotatedImageTitle(),getOption('thumb_size'),getOption('thumb_size'),getOption('thumb_size'),getOption('thumb_size'),getOption('thumb_size'),null,null,'remove-attributes',null,true);
-								} else { 
-								printImageThumb(getBareImageTitle(),'remove-attributes'); 
+								} else {
+								printImageThumb(getBareImageTitle(),'remove-attributes');
 								} ?>
 							</a>
 						<?php } ?>
 					</div>
 					<?php endwhile; ?>
 				</div>
-				
+
+				<div id="object-desc"><?php printAlbumDesc(); ?></div>
+
 				<div id="page-nav" class="clearfix">
 					<div class="jump"><?php printBaseAlbumMenuJump('count',gettext('Gallery Index')); ?></div>
 					<?php if ((hasNextPage()) || (hasPrevPage())) printPageListWithNav('« '.gettext('prev'),gettext('next').' »',false,true,'pagination'); ?>
 				</div>
-				
+
 				<?php printCodeblock(); ?>
-				
+
 				<?php if (getOption('zpbase_archive')) {
-				$singletag = getTags(); $tagstring = implode(', ', $singletag); 
+				$singletag = getTags(); $tagstring = implode(', ', $singletag);
 				if (strlen($tagstring) > 0) { ?>
 				<div class="block"><?php echo gettext('Tags: '); ?><?php printTags('links','','taglist', ', '); ?></div>
-				<?php } 
+				<?php }
 				} ?>
 
 				<?php if (function_exists('printRating')) { ?>
@@ -116,13 +117,13 @@ include ('inc/header.php'); ?>
 				<?php if ((function_exists('printAddToFavorites')) && ($_zp_gallery_page == 'album.php')) { ?>
 				<div id="favorites" class="block"><?php printAddToFavorites($_zp_current_album); ?></div>
 				<?php } ?>
-				
+
 				<?php if (getOption('zpbase_disqus')) { ?><div class="block"><?php printDisqusCommentForm(); ?></div>
 				<?php } elseif (function_exists('printCommentForm')) { ?><div class="block"><?php printCommentForm(); ?></div><?php } ?>
 				<?php if (function_exists('printRelatedItems')) { ?><div class="block"><?php printRelatedItems(5,'albums',null,null,'albums'); ?></div><?php } ?>
-				
+
 			</div>
 		</div>
-	</div> 
+	</div>
 
 <?php include ('inc/footer.php'); ?>
